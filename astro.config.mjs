9176a79edffff75ab68adb7f86ba4f28b2a34522
YugandharrPatil <yugandhar.patil@learner.manipal.edu>
2026-03-14 13:20:32 +0530

    ∙ contact page created and put contact form in there ∙ CTA now links to contact page env variables accessing syntax fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+	integrations: [react()],
 
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  output: "server",
-  adapter: vercel()
+	output: "server",
+	adapter: vercel(),
+	devToolbar: {
+		enabled: false,
+	},
+	env: {
+		schema: {
+			PUBLIC_SUPABASE_URL: envField.string({ context: "server", access: "public" }),
+			PUBLIC_SUPABASE_KEY: envField.string({ context: "server", access: "public" }),
+			GITHUB_AUTH_TOKEN: envField.string({ context: "server", access: "secret" }),
+		},
+	},
 });
