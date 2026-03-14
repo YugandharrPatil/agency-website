ef4464922fcb62c51f686e318744f55632c6e55f
YugandharrPatil <yugandhar.patil@learner.manipal.edu>
2026-03-14 13:20:53 +0530

    supabase env import syntax fixed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
+import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "astro:env/server";
+
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabaseKey = PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-        
